c0824137b8466307978d340d792c627c3bd1937d
Adrina Tang <adrina@Adrinas-MacBook-Pro.local>
2024-05-05 02:53:16 -0400

    fix datamodule bug

--- conplex_dti/dataset/datamodules.py
+++ conplex_dti/dataset/datamodules.py
@@ -54,8 +54,11 @@ def get_task_dir(task_name: str, database_root: Path):
 
 def get_contrastive_datamodule(datamodule_str):
     from .. import dataset as datamodules
-    
-    return getattr(datamodules, datamodule_str)
+    datamodule_dict = {
+        "dude": "DUDEDataModule",
+        "lit-pcba": "LIT_PCBADataModule",
+    }
+    return getattr(datamodules, datamodule_dict[datamodule_str])
 
 
 def drug_target_collate_fn(args: T.Tuple[torch.Tensor, torch.Tensor, torch.Tensor]):
